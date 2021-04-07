668445adec421a3ad122a8a9193173dce2e174c8
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-04-07 09:31:20 +0800

    create input binding two way data stream

--- src/app/form-data-stream/form-data-stream.component.ts
+++ src/app/form-data-stream/form-data-stream.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form-data-stream',
   templateUrl: './form-data-stream.component.html',
-  styleUrls: ['./form-data-stream.component.scss']
+  styleUrls: ['./form-data-stream.component.scss'],
 })
 export class FormDataStreamComponent implements OnInit {
+  name: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
