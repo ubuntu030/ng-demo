b60a627f7ded09bffc7fc0eb685864827f9cfc55
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-04-07 09:03:14 +0800

    apply form-data-stream

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ParentComponent } from './parent/parent.component';
+import { FormDataStreamComponent } from './form-data-stream/form-data-stream.component';
+
+const routes: Routes = [
+  { path: 'parent', component: ParentComponent },
+  { path: 'formData', component: FormDataStreamComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
