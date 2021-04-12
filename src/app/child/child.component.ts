4708dcdb725b5af9b4634d013f9baded58a886cd
ubuntu030 <ubuntu030@gmail.com>
2021-04-12 22:28:20 +0800

    父子元件 todo demo

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+
+import { Itodo } from '../todo';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent implements OnInit {
+  @Input() todos: Itodo[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Itodo } from '../todo';
+
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.scss']
+  styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent implements OnInit {
+  todos: Itodo[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  add(dscptn: Itodo['dscptn']): void {
+    this.todos.push({
+      id: new Date().getTime().toString(),
+      dscptn: dscptn,
+    });
   }
 
+  delete({ id }: Itodo): void {
+    this.todos.map((item): any => {
+      if (item.id !== id) {
+        return item;
+      }
+    });
+  }
 }
