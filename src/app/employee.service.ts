2fdfab0ae11db01f05655f20035f0c4b4121e4b4
ubuntu030 <ubuntu030@gmail.com>
2021-04-13 22:13:47 +0800

    更新資料

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -6,8 +6,24 @@ import { IEmployees } from './employee';
 })
 export class EmployeeService {
   employee: IEmployees[] = [
-    { id: '1', name: '加班君', skill: ['加班快樂!'] },
-    { id: '2', name: '耍廢君', skill: ['上班耍廢'] },
+    {
+      id: '1',
+      name: '加班君',
+      skill: ['加班快樂!'],
+      detail: '一間扛起一家七口，不加班就無法餬口',
+    },
+    {
+      id: '2',
+      name: '耍廢君',
+      skill: ['上班耍廢', '看U2'],
+      detail: '工作甚麼的不重要，就真的是來交朋友的',
+    },
+    {
+      id: '3',
+      name: '小蝦米',
+      skill: ['跑腿'],
+      detail: '就只是個蝦米..',
+    },
   ];
 
   constructor() {}
