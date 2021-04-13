c616df50543f759071506216a392c7851d1dc4b1
ubuntu030 <ubuntu030@gmail.com>
2021-04-13 22:32:13 +0800

    apply routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
 import { FormDataStreamComponent } from './form-data-stream/form-data-stream.component';
 import { RoutingComponent } from './routing/routing.component';
+import { StaffDetailComponent } from './staff-detail/staff-detail.component';
 
 const routes: Routes = [
   { path: 'parent', component: ParentComponent },
   { path: 'formData', component: FormDataStreamComponent },
   { path: 'routing', component: RoutingComponent },
+  { path: 'detail/:id', component: StaffDetailComponent },
 ];
 
 @NgModule({
