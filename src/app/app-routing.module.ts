9a3df76bbbb1a6ee7872905a54eea6c57478264b
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-04-14 09:59:53 +0800

    add 404 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ParentComponent } from './parent/parent.component';
 import { FormDataStreamComponent } from './form-data-stream/form-data-stream.component';
 import { RoutingComponent } from './routing/routing.component';
 import { StaffDetailComponent } from './staff-detail/staff-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/formData', pathMatch: 'full' },
@@ -12,6 +13,11 @@ const routes: Routes = [
   { path: 'formData', component: FormDataStreamComponent },
   { path: 'routing', component: RoutingComponent },
   { path: 'detail/:id', component: StaffDetailComponent },
+  // 萬用字元路由
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
