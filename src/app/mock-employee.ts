347f882b2f1e1dd34569c8501637e2872db2e040
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-04-14 11:25:17 +0800

    add mock data

---
+++ src/app/mock-employee.ts
@@ -0,0 +1,22 @@
+import { IEmployees } from './employee';
+
+export const EMPLOYEES: IEmployees[] = [
+  {
+    id: '1',
+    name: '加班君',
+    skill: ['加班快樂!'],
+    detail: '一間扛起一家七口，不加班就無法餬口',
+  },
+  {
+    id: '2',
+    name: '耍廢君',
+    skill: ['上班耍廢', '看U2'],
+    detail: '工作甚麼的不重要，就真的是來交朋友的',
+  },
+  {
+    id: '3',
+    name: '小蝦米',
+    skill: ['跑腿'],
+    detail: '就只是個蝦米..',
+  },
+];
