7dde0d8b17ae4685f106d914f4171d5362c877fd
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-04-14 11:25:22 +0800

    update

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,35 +1,18 @@
 import { Injectable } from '@angular/core';
 import { IEmployees } from './employee';
 
+import { EMPLOYEES } from './mock-employee';
+
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  employee: IEmployees[] = [
-    {
-      id: '1',
-      name: '加班君',
-      skill: ['加班快樂!'],
-      detail: '一間扛起一家七口，不加班就無法餬口',
-    },
-    {
-      id: '2',
-      name: '耍廢君',
-      skill: ['上班耍廢', '看U2'],
-      detail: '工作甚麼的不重要，就真的是來交朋友的',
-    },
-    {
-      id: '3',
-      name: '小蝦米',
-      skill: ['跑腿'],
-      detail: '就只是個蝦米..',
-    },
-  ];
+  employees = EMPLOYEES;
 
   constructor() {}
 
   getStaff(id: IEmployees['id']): IEmployees | undefined {
-    const staffArr = this.employee.filter((item) => item.id === id);
+    const staffArr = this.employees.filter((item) => item.id === id);
     if (staffArr && staffArr.length > 0) {
       return staffArr[0];
     }
@@ -37,6 +20,6 @@ export class EmployeeService {
   }
 
   getStaffs(): IEmployees[] {
-    return this.employee;
+    return this.employees;
   }
 }
