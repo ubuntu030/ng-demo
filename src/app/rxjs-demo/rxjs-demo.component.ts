ca35cad32c9be30abdff85647d07f34473c4006a
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-05-04 16:20:35 +0800

    sort table

--- src/app/rxjs-demo/rxjs-demo.component.ts
+++ src/app/rxjs-demo/rxjs-demo.component.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '@angular/common/http';
 
 import { EmployeeService } from '../employee.service';
 
-import { Observable, throwError } from 'rxjs';
-import { catchError, pluck, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, map, pluck, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-demo',
@@ -14,7 +14,12 @@ import { catchError, pluck, tap } from 'rxjs/operators';
 export class RxjsDemoComponent implements OnInit {
   constructor(private employeeService: EmployeeService) {}
   tableList = [];
+  tableList$ = new BehaviorSubject<any[]>([]);
 
+  sortSate = {
+    type: '',
+    sort: 1,
+  };
   ngOnInit(): void {
     const self = this;
   }
@@ -24,10 +29,39 @@ export class RxjsDemoComponent implements OnInit {
     self.employeeService.getEmployees().subscribe({
       next: (data) => {
         console.log(data);
+        self.tableList$.next(data);
       },
       error: (error) => {
         console.log(error);
       },
     });
+  }
+
+  filterDataList(key: string): void {
+    const self = this;
+    self.tableList$
+      .pipe(map((val) => self.sortTable(val, key)))
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+  sortTable(data: any, key: string): void {
+    const sortSate = this.sortSate;
+    if (sortSate.type === key) {
+      sortSate.sort = -sortSate.sort;
+    } else {
+      sortSate.type = key;
+    }
+    const newData = data.sort((a: any, b: any) => {
+      if (a[key] < b[key]) {
+        return sortSate.sort;
+      }
+      if (a[key] > b[key]) {
+        return -sortSate.sort;
+      }
+      return 0;
+    });
+    return newData;
   }
 }
