faae702e339f5506f796a6d747d01f0edcac59a3
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-05-05 10:20:29 +0800

    新增

--- src/app/rxjs-demo/rxjs-demo.component.ts
+++ src/app/rxjs-demo/rxjs-demo.component.ts
@@ -25,6 +25,8 @@ export class RxjsDemoComponent implements OnInit, AfterViewInit {
 
   searchInput$!: Observable<any>;
 
+  private locationArr = ['高雄', '台南', '嘉義', '台中', '苗栗', '台北'];
+  private favoriteArr = ['網球', '登山', '羽球', '籃球'];
   sortSate = {
     type: '',
     sort: 1,
@@ -37,7 +39,7 @@ export class RxjsDemoComponent implements OnInit, AfterViewInit {
     const self = this;
     self.searchInput$ = fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
       debounceTime(500),
-      map((val: any) => val.target.value),
+      map((val: any) => val.target.value)
     );
     self.searchInput$.subscribe((res) => console.log(res));
   }
@@ -81,5 +83,36 @@ export class RxjsDemoComponent implements OnInit, AfterViewInit {
       return 0;
     });
     return newData;
+  }
+
+  addNewData(): void {
+    const self = this;
+    self.tableList$
+      .pipe(
+        map((data) => data.sort(() => 1)),
+        map((data) => data.push(self.createData(data))),
+        tap((data) => console.log(data))
+      )
+      .subscribe()
+      .unsubscribe();
+  }
+
+  createData(data: any): any {
+    const self = this;
+    return {
+      ...data[data.length - 1],
+      id: data[data.length - 1].id + 1,
+      name: 'A' + data[data.length - 1].id + 1,
+      address: self.randomLocation(),
+      favorite: self.randomFavorite(),
+    };
+  }
+  randomLocation(): string {
+    const locationArr = this.locationArr;
+    return locationArr[Math.floor(Math.random() * locationArr.length)];
+  }
+  randomFavorite(): string {
+    const favoriteArr = this.favoriteArr;
+    return favoriteArr[Math.floor(Math.random() * favoriteArr.length)];
   }
 }
