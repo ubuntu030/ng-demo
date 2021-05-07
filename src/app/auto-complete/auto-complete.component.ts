9eed02f8eadc5179fd047ad42205f4de8f6ca96c
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-05-07 16:32:10 +0800

    auto complete initial

--- src/app/auto-complete/auto-complete.component.ts
+++ src/app/auto-complete/auto-complete.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+const URL =
+  'https://zh.wikipedia.org/w/api.php?action=opensearch&format=json&limit=5&origin=*';
 
 @Component({
   selector: 'app-auto-complete',
   templateUrl: './auto-complete.component.html',
-  styleUrls: ['./auto-complete.component.scss']
+  styleUrls: ['./auto-complete.component.scss'],
 })
-export class AutoCompleteComponent implements OnInit {
+export class AutoCompleteComponent implements OnInit, AfterViewInit {
+  @ViewChild('searchInpu') searchInpu!: ElementRef;
+  @ViewChild('suggestList') suggestList!: ElementRef;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {}
 
-  ngOnInit(): void {
+  getSuggestList(keyword: string) {
+    fetch(URL + '&search=' + keyword, {
+      method: 'GET',
+      mode: 'cors',
+    }).then((res) => res.json());
   }
-
 }
